0bfc46f895c26e7e1663ff1c44fd1a153cf1ba5f
Gang Yang <neil_young.7up@outlook.com>
2023-07-14 16:41:23 +0930

    feat: add router view

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppSidebar from './components/AppSidebar/AppSidebar.vue'
+</script>
 
-<template></template>
+<template>
+  <div class="flex flex-col md:flex-row">
+    <div class="w-64 bg-gray-50 dark:bg-gray-800">
+      <AppSidebar />
+    </div>
+    <div class="flex-grow bg-white dark:bg-gray-900 pt-5 ml-5">
+      <router-view />
+    </div>
+  </div>
+</template>
 
 <style></style>
