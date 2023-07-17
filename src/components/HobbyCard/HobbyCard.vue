c5d79b609632661e2fea050dd0d7e4a63af6ec2a
Gang Yang <neil_young.7up@outlook.com>
2023-07-18 00:02:23 +0930

    feat: add hobbies page (#8)
    * feat: add hobbies page
    
    * feat: minor change

---
+++ src/components/HobbyCard/HobbyCard.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+const props = defineProps({
+  imgUrl: String,
+  desc: String,
+  name: String,
+})
+
+const openModal = () => {
+  const modal = document.createElement('div')
+  modal.classList.add(
+    'fixed',
+    'top-0',
+    'left-0',
+    'w-screen',
+    'h-screen',
+    'bg-black',
+    'bg-opacity-50',
+    'z-50',
+    'flex',
+    'justify-center',
+    'items-center',
+  )
+  modal.innerHTML = `
+    <div class="max-w-2xl rounded-lg bg-white p-4">
+      <h3 class="text-lg font-semibold mb-2">${props.name}</h3>
+      <p class="text-gray-600">${props.desc}</p>
+    </div>
+  `
+  modal.addEventListener('click', () => {
+    modal.remove()
+  })
+  document.body.appendChild(modal)
+}
+</script>
+
+<template>
+  <figure
+    class="relative max-w-2xl cursor-pointer grayscale filter transition-all duration-300 hover:grayscale-0"
+  >
+    <button @click="openModal">
+      <img class="rounded-lg" :src="props.imgUrl" alt="image description" />
+    </button>
+  </figure>
+</template>
--- src/pages/Hobbies/Hobbies.vue
+++ src/pages/Hobbies/Hobbies.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import PageHeader from '@/components/PageHeader/PageHeader.vue'
+import HobbyCard from '@/components/HobbyCard/HobbyCard.vue'
+import hobbyData from '@/data/hobbies.json'
 </script>
 
 <template>
@@ -9,6 +11,19 @@ import PageHeader from '@/components/PageHeader/PageHeader.vue'
         <template #pageHeaderText>HOBBIES</template>
       </PageHeader>
     </div>
-    <div class="flex-grow bg-gray-200"></div>
+    <div class="flex-grow bg-gray-200">
+      <div class="mb-4 mt-4 flex justify-center">
+        <div class="mx-auto my-0 md:flex">
+          <HobbyCard
+            v-for="hobby in hobbyData"
+            :key="hobby.imgUrl"
+            :imgUrl="hobby.imgUrl"
+            :desc="hobby.desc"
+            :name="hobby.name"
+            class="m-8"
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
